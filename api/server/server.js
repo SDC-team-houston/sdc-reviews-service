94564f5f2b29a6bdb34070072de08da29f182af7
Christian Peterson <25235970+ChrisRPeterson@users.noreply.github.com>
2021-05-13 10:25:50 -0500

    Added file to loader.io verify file

--- api/server/server.js
+++ api/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const reviews = require('./routes/reviews');
 require('./db');
 
@@ -11,6 +12,12 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/reviews', reviews);
+
+app.get('/loaderio-99c02dac162d5c22343b94833afbd2cc.txt', (req, res) => {
+  res.sendFile('loaderio-99c02dac162d5c22343b94833afbd2cc.txt', {
+    root: path.join(__dirname, 'assets'),
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server listening on PORT: ${port}`);
